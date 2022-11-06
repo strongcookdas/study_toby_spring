51832ef26ff2bb47dbb05df4f9e828a71019756e
strongcookdas <goodjav141592@gmail.com>
2022-11-06 22:27:42 +0900

    build : UserDao와 UserDaoTest에 spring 적용

--- src/main/java/tody_spring/UserDaoFactory.java
+++ src/main/java/tody_spring/UserDaoFactory.java
@@ -1,8 +1,13 @@
 package tody_spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import tody_spring.connection.AWSConnectionMaker;
 
+@Configuration
 public class UserDaoFactory {
+
+    @Bean
     public UserDao awsUserDao(){
         return new UserDao(new AWSConnectionMaker());
     }
--- src/test/java/tody_spring/UserDaoTest.java
+++ src/test/java/tody_spring/UserDaoTest.java
@@ -2,19 +2,28 @@ package tody_spring;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tody_spring.connection.AWSConnectionMaker;
 import tody_spring.domain.User;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
     @Test
     @DisplayName("addAndGet 테스트")
     void addAndGet()throws SQLException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User insert = new User("3","kim","0000");
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        User insert = new User("4","kim","0000");
         userDao.add(insert);
         User select = userDao.findById(insert.getId());
         assertEquals(insert.getId(),select.getId());
